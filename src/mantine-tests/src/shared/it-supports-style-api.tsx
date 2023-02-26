4b8ece209327377814ac50885a06696738af2d67
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-26 16:05:41 +0400

    Add shared styles api styles test

--- src/mantine-tests/src/shared/it-supports-style-api.tsx
+++ src/mantine-tests/src/shared/it-supports-style-api.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from '../render';
 
+const getRandomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+const getTestClassNames = (selectors: string[]) =>
+  selectors.reduce<Record<string, string>>((acc, selector) => {
+    acc[selector] = `test-${selector}`;
+    return acc;
+  }, {});
+
 interface Options<Props extends Record<string, any> = any, Selectors extends string = string> {
   component: React.ComponentType<Props>;
   props: Props;
@@ -12,15 +19,32 @@ export function itSupportsStylesApi<
   Selectors extends string = string
 >(options: Options<Props, Selectors>, name = 'supports styles api') {
   it(`${name}: classNames`, () => {
-    const classNames = options.selectors.reduce<Record<string, string>>((acc, selector) => {
-      acc[selector] = `test-${selector}`;
-      return acc;
-    }, {});
-
+    const classNames = getTestClassNames(options.selectors);
     const { container } = render(<options.component {...options.props} classNames={classNames} />);
 
     options.selectors.forEach((selector) => {
       expect(container.querySelector(`.${classNames[selector]}`)).toBeInTheDocument();
+    });
+  });
+
+  it(`${name}: styles`, () => {
+    const classNames = getTestClassNames(options.selectors);
+    const styles = options.selectors.reduce<Record<string, React.CSSProperties>>(
+      (acc, selector) => {
+        acc[selector] = { color: `#${getRandomColor()}` };
+        return acc;
+      },
+      {}
+    );
+
+    const { container } = render(
+      <options.component {...options.props} classNames={classNames} styles={styles} />
+    );
+
+    options.selectors.forEach((selector) => {
+      expect(container.querySelector(`.${classNames[selector]}`)).toHaveStyle({
+        ...styles[selector],
+      });
     });
   });
 }
