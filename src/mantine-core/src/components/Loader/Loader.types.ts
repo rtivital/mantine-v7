cce38a36c8a93e2477b031bdeea2abfd51c8a622
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-27 11:59:29 +0400

    Add less strict types to Loader

--- src/mantine-core/src/components/Loader/Loader.types.ts
+++ src/mantine-core/src/components/Loader/Loader.types.ts
@@ -1,7 +1,7 @@
 import type { CssVarsProp } from '../../core';
 import type { LoaderCssVariables } from './Loader';
 
-export type MantineLoader = 'bars' | 'dots' | 'oval';
+export type MantineLoader = 'bars' | 'dots' | 'oval' | (string & {});
 
 export interface SvgLoaderProps extends React.ComponentPropsWithoutRef<'svg'> {
   vars?: CssVarsProp<LoaderCssVariables>;
