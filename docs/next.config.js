860f19717a6b5dabc2ed6977f0876f922f1b1526
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-27 14:03:01 +0400

    Add remark mdx plugin

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,9 +1,10 @@
+const remarkSlug = require('remark-slug');
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     providerImportSource: '@mdx-js/react',
-    remarkPlugins: [],
-    rehypePlugins: [],
+    remarkPlugins: [remarkSlug],
   },
 });
 
