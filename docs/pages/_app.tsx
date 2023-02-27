f68ae48b19e631cc0fd3ba3720507567c206a33d
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-27 14:30:18 +0400

    Init app initial props in docs

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -4,7 +4,8 @@ import type { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import { MdxProvider } from '@/components/MdxProvider';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, ...others }: AppProps) {
+  console.log(others);
   return (
     <MantineProvider>
       <MdxProvider>
@@ -13,3 +14,10 @@ export default function App({ Component, pageProps }: AppProps) {
     </MantineProvider>
   );
 }
+
+export const getInitialProps = async () =>
+  Promise.resolve({
+    hello: true,
+  });
+
+App.getInitialProps = getInitialProps;
