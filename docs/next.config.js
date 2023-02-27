a681cb169eb62b8c572b6f83d28fbff1c835393b
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-27 13:56:00 +0400

    Init mdx for docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,16 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    providerImportSource: '@mdx-js/react',
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['tsx', 'mdx'],
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
