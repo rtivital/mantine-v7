97111a177a8add1846e62dca1c115402dffd4b9b
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-28 21:20:28 +0400

    Add highlight theme

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import { MdxProvider } from '@/components/MdxProvider';
+import '../styles/highlight-theme.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { UnstyledButton } from '@mantine/core';
+import { CodeBlock } from '@/components/CodeBlock/CodeBlock';
 
 export default function Home() {
-  return <UnstyledButton bg="blue">Hello</UnstyledButton>;
+  return (
+    <>
+      <CodeBlock />
+      <UnstyledButton bg="blue">Hello</UnstyledButton>
+    </>
+  );
 }
