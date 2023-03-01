1a302a816c9662fd00a5b49abec9126a84641b49
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-01 13:29:26 +0400

    Add to-rgba, lighten, darken and rgba color functions

--- src/mantine-core/src/core/MantineProvider/color-functions/index.ts
+++ src/mantine-core/src/core/MantineProvider/color-functions/index.ts
@@ -1,2 +1,8 @@
 export { getPrimaryShade } from './get-primary-shade/get-primary-shade';
 export { parseThemeColor } from './parse-theme-color/parse-theme-color';
+export { toRgba } from './to-rgba/to-rgba';
+export { rgba } from './rgba/rgba';
+export { darken } from './darken/darken';
+export { lighten } from './lighten/lighten';
+
+export type { RGBA } from './to-rgba/to-rgba';
