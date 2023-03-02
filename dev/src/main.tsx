c9ec7da06a200c211d984f820c1032c3891ebbc6
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-02 09:58:03 +0400

    Remove old vite environment

--- dev/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
--- dev/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
-});
