b18c377c36d07b418021d880a35d56b69d534ce2
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-07 15:12:44 +0400

    Add UnstyledButton styles api

---
+++ src/mantine-styles-api/src/data/UnstyledButton.styles-api.ts
@@ -0,0 +1,8 @@
+import type { UnstyledButtonFactory } from '@mantine/core';
+import type { StylesApiData } from '../types';
+
+export const UnstyledButtonStylesApi: StylesApiData<UnstyledButtonFactory> = {
+  selectors: {
+    root: 'Root element',
+  },
+};
--- src/mantine-styles-api/src/index.ts
+++ src/mantine-styles-api/src/index.ts
@@ -2,3 +2,4 @@ export * from './types';
 
 export * from './data/ActionIcon.styles-api';
 export * from './data/Loader.styles-api';
+export * from './data/UnstyledButton.styles-api';
