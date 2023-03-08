19a0407a2615bbc154066e31302690aeab1e059d
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-08 15:13:28 +0400

    Add Transition component

---
+++ src/mantine-core/src/components/Transition/index.ts
@@ -0,0 +1,4 @@
+export { transitions as MANTINE_TRANSITIONS } from './transitions';
+export { Transition } from './Transition';
+export type { TransitionProps, TransitionOverride } from './Transition';
+export type { MantineTransition } from './transitions';
--- src/mantine-core/src/components/index.ts
+++ src/mantine-core/src/components/index.ts
@@ -2,4 +2,5 @@ export * from './ActionIcon';
 export * from './Collapse';
 export * from './Loader';
 export * from './Portal';
+export * from './Transition';
 export * from './UnstyledButton';
