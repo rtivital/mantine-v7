69da354e3587439e9290e2a01912b39e307be8b7
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-11 17:03:45 +0400

    Add Anchor component styles api

--- src/mantine-styles-api/src/data/Text.styles-api.ts
+++ src/mantine-styles-api/src/data/Text.styles-api.ts
@@ -15,4 +15,17 @@ export const TextStylesApi: StylesApiData<TextFactory> = {
     '--gradient': 'Text fill gradient, controlled by gradient prop',
     '--line-clamp': 'Number of lines that should be visible, controlled by lineClamp prop',
   },
+
+  modifiers: [
+    { modifier: 'data-variant', selector: 'root', value: 'Value of variant prop' },
+    {
+      modifier: 'data-truncate',
+      selector: 'root',
+      value: 'Value of truncate prop',
+      condition: 'truncate prop is set',
+    },
+    { modifier: 'data-line-clamp', selector: 'root', condition: 'lineClamp prop is a number' },
+    { modifier: 'data-inline', selector: 'root', condition: 'inline prop is set' },
+    { modifier: 'data-inherit', selector: 'root', condition: 'inherit prop is set' },
+  ],
 };
--- src/mantine-styles-api/src/index.ts
+++ src/mantine-styles-api/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 
 export * from './data/ActionIcon.styles-api';
+export * from './data/Anchor.styles-api';
 export * from './data/Loader.styles-api';
 export * from './data/Text.styles-api';
 export * from './data/UnstyledButton.styles-api';
