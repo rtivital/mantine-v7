eac3b07cc6067f917a9bf192ae5dad5e651cefba
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-11 18:47:07 +0400

    Add CloseButton styles api

---
+++ src/mantine-styles-api/src/data/CloseButton.styles-api.ts
@@ -0,0 +1,11 @@
+import type { CloseButtonFactory } from '@mantine/core';
+import type { StylesApiData } from '../types';
+import { ActionIconStylesApi } from './ActionIcon.styles-api';
+
+export const CloseButtonStylesApi: StylesApiData<CloseButtonFactory> = {
+  ...ActionIconStylesApi,
+  vars: {
+    ...ActionIconStylesApi.vars,
+    '--icon-size': 'Size of close icon, controlled by iconSize prop',
+  },
+};
--- src/mantine-styles-api/src/index.ts
+++ src/mantine-styles-api/src/index.ts
@@ -2,6 +2,7 @@ export * from './types';
 
 export * from './data/ActionIcon.styles-api';
 export * from './data/Anchor.styles-api';
+export * from './data/CloseButton.styles-api';
 export * from './data/Loader.styles-api';
 export * from './data/Text.styles-api';
 export * from './data/UnstyledButton.styles-api';
