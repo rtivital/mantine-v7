c9a18d6684dedaa88e635b16a2cbc934326cdd7e
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-18 11:09:12 +0400

    Add Tabs component exports

---
+++ src/mantine-core/src/components/Tabs/index.ts
@@ -0,0 +1,13 @@
+export { Tabs } from './Tabs';
+export type {
+  TabsProps,
+  TabsCssVariables,
+  TabsFactory,
+  TabsStylesNames,
+  TabsStylesParams,
+  TabsVariant,
+} from './Tabs';
+
+export type { TabsTabProps, TabsTabStylesNames } from './TabsTab/TabsTab';
+export type { TabsPanelProps, TabsPanelStylesNames } from './TabsPanel/TabsPanel';
+export type { TabsListProps, TabsListStylesNames } from './TabsList/TabsList';
