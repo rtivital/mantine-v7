b346355ec4af54f58bc0798d8df032b0373db6b0
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-19 15:45:19 +0400

    Add styles api tests to Table

--- src/mantine-core/src/components/Table/Table.test.tsx
+++ src/mantine-core/src/components/Table/Table.test.tsx
@@ -2,18 +2,44 @@ import React from 'react';
 import { render, tests } from '@mantine/tests';
 import { Table, TableProps, TableStylesNames } from './Table';
 
-const defaultProps: TableProps = {};
+const defaultProps: TableProps = {
+  children: (
+    <>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>test-th-1</Table.Th>
+          <Table.Th>test-th-2</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+
+      <Table.Tbody>
+        <Table.Tr>
+          <Table.Td>test-td-1</Table.Td>
+          <Table.Td>test-td-2</Table.Td>
+        </Table.Tr>
+      </Table.Tbody>
+
+      <Table.Tfoot>
+        <Table.Tr>
+          <Table.Td>test-td-1</Table.Td>
+          <Table.Td>test-td-2</Table.Td>
+        </Table.Tr>
+      </Table.Tfoot>
+
+      <Table.Caption>test-caption</Table.Caption>
+    </>
+  ),
+};
 
 describe('@mantine/core/Table', () => {
   tests.itSupportsSystemProps<TableProps, TableStylesNames>({
     component: Table,
     props: defaultProps,
-    polymorphic: true,
     styleProps: true,
     extend: true,
     refType: HTMLTableElement,
     displayName: '@mantine/core/Table',
-    stylesApiSelectors: ['table'],
+    stylesApiSelectors: ['table', 'caption', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td'],
   });
 
   it('renders children', () => {
