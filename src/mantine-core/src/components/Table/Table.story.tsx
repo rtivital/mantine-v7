1ce19e73ad0d59e38d3ae6bdd21eb02a08b767f5
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-19 13:02:38 +0400

    Add Table props

---
+++ src/mantine-core/src/components/Table/Table.story.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Table } from './Table';
+
+export default { title: 'Table' };
+
+const elements = [
+  { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
+  { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
+  { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
+  { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
+  { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+];
+
+const rows = elements.map((element) => (
+  <Table.Tr key={element.name}>
+    <Table.Td>{element.position}</Table.Td>
+    <Table.Td>{element.name}</Table.Td>
+    <Table.Td>{element.symbol}</Table.Td>
+    <Table.Td>{element.mass}</Table.Td>
+  </Table.Tr>
+));
+
+export function Usage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Element position</Table.Th>
+            <Table.Th>Element name</Table.Th>
+            <Table.Th>Symbol</Table.Th>
+            <Table.Th>Atomic mass</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </div>
+  );
+}
--- src/mantine-core/src/components/Table/Table.tsx
+++ src/mantine-core/src/components/Table/Table.tsx
@@ -7,6 +7,9 @@ import {
   ElementProps,
   useComponentDefaultProps,
   useStylesApi,
+  useComponentVars,
+  MantineColor,
+  MantineSpacing,
 } from '../../core';
 import {
   TableCaption,
@@ -31,14 +34,51 @@ export type TableStylesNames =
   | 'caption';
 
 export type TableVariant = string;
-export type TableCssVariables = '--test';
+export type TableCssVariables =
+  | '--table-caption-side'
+  | '--table-horizontal-spacing'
+  | '--table-vertical-spacing';
 
-export interface TableStylesParams {}
+export interface TableStylesParams {
+  captionSide: 'top' | 'bottom' | undefined;
+  horizontalSpacing: MantineSpacing | number | string | undefined;
+  verticalSpacing: MantineSpacing | number | string | undefined;
+}
 
 export interface TableProps
   extends BoxProps,
     StylesApiProps<TableStylesNames, TableVariant, TableCssVariables>,
-    ElementProps<'table'> {}
+    ElementProps<'table'> {
+  /** Determines on which side `Table.Caption` will be displayed, `bottom` by default */
+  captionSide?: 'top' | 'bottom';
+
+  /** Determines whether table should have outer border, `false` by default */
+  withTableBorder?: boolean;
+
+  /** Determines whether table should have borders between columns, `false` by default */
+  withColumnBorders?: boolean;
+
+  /** Determines whether table should have borders between rows, `true` by default */
+  withRowBorders?: boolean;
+
+  /** Horizontal cells spacing, key of `theme.spacing` or any valid CSS value for padding, numbers are converted to rem (1rem = 16px), default value is `xs` */
+  horizontalSpacing?: MantineSpacing | number | string;
+
+  /** Vertical cells spacing, key of `theme.spacing` or any valid CSS value for padding, numbers are converted to rem (1rem = 16px), default value is `xs` */
+  verticalSpacing?: MantineSpacing | number | string;
+
+  /** Determines whether every odd/even row background should be changed to `strippedColor`, if set to `true`, then `odd` value will be used, `false` by default  */
+  striped?: boolean | 'odd' | 'even';
+
+  /** Background color of striped rows, key of `theme.colors` or any valid CSS color, by default color is inferred from current color scheme */
+  stripedColor?: MantineColor | string;
+
+  /** Determines whether table rows background should change to `highlightOnHoverColor` when hovered, `false` by default */
+  highlightOnHover?: boolean;
+
+  /** Background color of table rows when hovered, key of `theme.colors` or any valid CSS color, by default color is inferred from current color scheme */
+  highlightOnHoverColor?: MantineColor | string;
+}
 
 export interface TableFactory {
   props: TableProps;
@@ -57,14 +97,30 @@ export interface TableFactory {
   };
 }
 
-const defaultProps: Partial<TableProps> = {};
+const defaultProps: Partial<TableProps> = {
+  captionSide: 'bottom',
+  withRowBorders: true,
+};
 
 export const Table = factory<TableFactory>((props, ref) => {
-  const { classNames, className, style, styles, unstyled, ...others } = useComponentDefaultProps(
-    'Table',
-    defaultProps,
-    props
-  );
+  const {
+    classNames,
+    className,
+    style,
+    styles,
+    unstyled,
+    vars,
+    horizontalSpacing,
+    verticalSpacing,
+    captionSide,
+    ...others
+  } = useComponentDefaultProps('Table', defaultProps, props);
+
+  const _vars = useComponentVars<TableStylesParams>('Table', vars, {
+    captionSide,
+    horizontalSpacing,
+    verticalSpacing,
+  });
 
   const getStyles = useStylesApi({
     name: 'Table',
@@ -78,7 +134,18 @@ export const Table = factory<TableFactory>((props, ref) => {
 
   return (
     <TableProvider value={{ getStyles }}>
-      <Box component="table" ref={ref} {...getStyles('root')} {...others} />
+      <Box
+        component="table"
+        ref={ref}
+        vars={{
+          '--table-caption-side': captionSide,
+          '--table-horizontal-spacing': horizontalSpacing,
+          '--table-vertical-spacing': verticalSpacing,
+          ..._vars,
+        }}
+        {...getStyles('root')}
+        {...others}
+      />
     </TableProvider>
   );
 });
