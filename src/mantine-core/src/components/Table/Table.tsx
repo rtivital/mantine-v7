48e73fcb9dac348ffcb0b4682cc6764a10a493cd
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-19 13:16:26 +0400

    Add Table css variables and styles params

--- src/mantine-core/src/components/Table/Table.tsx
+++ src/mantine-core/src/components/Table/Table.tsx
@@ -11,6 +11,8 @@ import {
   MantineColor,
   MantineSpacing,
   getSpacing,
+  useMantineTheme,
+  getThemeColor,
 } from '../../core';
 import {
   TableCaption,
@@ -38,28 +40,32 @@ export type TableVariant = string;
 export type TableCssVariables =
   | '--table-caption-side'
   | '--table-horizontal-spacing'
-  | '--table-vertical-spacing';
+  | '--table-vertical-spacing'
+  | '--table-striped-color'
+  | '--table-highlight-on-hover-color';
 
 export interface TableStylesParams {
   captionSide: 'top' | 'bottom' | undefined;
   horizontalSpacing: MantineSpacing | number | string | undefined;
   verticalSpacing: MantineSpacing | number | string | undefined;
+  stripedColor: MantineColor | string | undefined;
+  highlightOnHoverColor: MantineColor | string | undefined;
 }
 
 export interface TableProps
   extends BoxProps,
     StylesApiProps<TableStylesNames, TableVariant, TableCssVariables>,
     ElementProps<'table'> {
-  /** Determines on which side `Table.Caption` will be displayed, `bottom` by default */
+  /** Determines on which side `Table.Caption` is displayed, `bottom` by default */
   captionSide?: 'top' | 'bottom';
 
-  /** Determines whether table should have outer border, `false` by default */
+  /** Determines whether the table should have outer border, `false` by default */
   withTableBorder?: boolean;
 
-  /** Determines whether table should have borders between columns, `false` by default */
+  /** Determines whether the table should have borders between columns, `false` by default */
   withColumnBorders?: boolean;
 
-  /** Determines whether table should have borders between rows, `true` by default */
+  /** Determines whether the table should have borders between rows, `true` by default */
   withRowBorders?: boolean;
 
   /** Horizontal cells spacing, key of `theme.spacing` or any valid CSS value for padding, numbers are converted to rem (1rem = 16px), default value is `xs` */
@@ -114,13 +120,21 @@ export const Table = factory<TableFactory>((props, ref) => {
     horizontalSpacing,
     verticalSpacing,
     captionSide,
+    stripedColor,
+    highlightOnHoverColor,
+    striped,
+    highlightOnHover,
     ...others
   } = useComponentDefaultProps('Table', defaultProps, props);
+
+  const theme = useMantineTheme();
 
   const _vars = useComponentVars<TableStylesParams>('Table', vars, {
     captionSide,
     horizontalSpacing,
     verticalSpacing,
+    stripedColor,
+    highlightOnHoverColor,
   });
 
   const getStyles = useStylesApi({
@@ -142,6 +156,12 @@ export const Table = factory<TableFactory>((props, ref) => {
           '--table-caption-side': captionSide,
           '--table-horizontal-spacing': getSpacing(horizontalSpacing),
           '--table-vertical-spacing': getSpacing(verticalSpacing),
+          '--table-striped-color':
+            striped && stripedColor ? getThemeColor(stripedColor, theme) : undefined,
+          '--table-highlight-on-hover-color':
+            highlightOnHover && highlightOnHoverColor
+              ? getThemeColor(highlightOnHoverColor, theme)
+              : undefined,
           ..._vars,
         }}
         {...getStyles('root')}
