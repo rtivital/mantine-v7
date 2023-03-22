1948ac2396ac283898cdaf620045d00612766ae7
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-22 11:28:19 +0400

    Add Paper styles api

---
+++ src/mantine-styles-api/src/data/Paper.styles-api.ts
@@ -0,0 +1,19 @@
+import type { PaperFactory } from '@mantine/core';
+import type { StylesApiData } from '../types';
+
+export const PaperStylesApi: StylesApiData<PaperFactory> = {
+  selectors: {
+    root: 'Root element',
+  },
+
+  vars: {
+    '--paper-radius': 'Border radius',
+    '--paper-shadow': 'Box shadow',
+  },
+
+  params: ['radius', 'shadow'],
+
+  modifiers: [
+    { modifier: 'data-with-border', selector: 'root', condition: 'withBorderProp is set' },
+  ],
+};
