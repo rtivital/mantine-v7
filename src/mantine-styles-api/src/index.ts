4abcc81a51b9dd5f3b8b20f5197aec356644f929
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-22 10:47:27 +0400

    Add ScrollArea styles api

--- src/mantine-styles-api/src/index.ts
+++ src/mantine-styles-api/src/index.ts
@@ -4,6 +4,7 @@ export * from './data/ActionIcon.styles-api';
 export * from './data/Anchor.styles-api';
 export * from './data/CloseButton.styles-api';
 export * from './data/Loader.styles-api';
+export * from './data/ScrollArea.styles-api';
 export * from './data/Tabs.styles-api';
 export * from './data/Table.styles-api';
 export * from './data/Text.styles-api';
