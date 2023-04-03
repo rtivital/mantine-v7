83c608112327b28100f2f36a624a8a7c101aa7e2
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-03 12:48:39 +0400

    Add Swatches component

---
+++ src/mantine-core/src/components/ColorPicker/Swatches/Swatches.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import {
+  Box,
+  BoxProps,
+  StylesApiProps,
+  factory,
+  ElementProps,
+  useProps,
+  useStyles,
+} from '../../../core';
+import { ColorSwatch } from '../../ColorSwatch';
+import classes from './Swatches.module.css';
+
+export type SwatchesStylesNames = 'swatches' | 'swatch';
+
+export interface SwatchesProps
+  extends BoxProps,
+    StylesApiProps<SwatchesStylesNames>,
+    ElementProps<'div'> {
+  size?: string | number;
+  data: string[];
+  swatchesPerRow?: number;
+  focusable?: boolean;
+  onChangeEnd?: (color: string) => void;
+  __staticSelector?: string;
+  setValue(value: string): void;
+}
+
+export interface SwatchesFactory {
+  props: SwatchesProps;
+  ref: HTMLDivElement;
+  stylesNames: SwatchesStylesNames;
+}
+
+const defaultProps: Partial<SwatchesProps> = {};
+
+export const Swatches = factory<SwatchesFactory>((props, ref) => {
+  const {
+    classNames,
+    className,
+    style,
+    styles,
+    unstyled,
+    vars,
+    datatype,
+    setValue,
+    onChangeEnd,
+    size,
+    focusable,
+    data,
+    swatchesPerRow,
+    ...others
+  } = useProps('Swatches', defaultProps, props);
+
+  const getStyles = useStyles<SwatchesStylesNames>({
+    name: 'Swatches',
+    className,
+    style,
+    classes,
+    classNames,
+    styles,
+    unstyled,
+  });
+
+  const colors = data.map((color, index) => (
+    <ColorSwatch
+      {...getStyles('swatch')}
+      component="button"
+      type="button"
+      color={color}
+      key={index}
+      radius="sm"
+      onClick={() => {
+        setValue(color);
+        onChangeEnd?.(color);
+      }}
+      aria-label={color}
+      tabIndex={focusable ? 0 : -1}
+    />
+  ));
+
+  return (
+    <Box
+      {...getStyles('swatches')}
+      ref={ref}
+      vars={{
+        '--swatch-size': `${100 / swatchesPerRow!}%`,
+      }}
+      {...others}
+    >
+      {colors}
+    </Box>
+  );
+});
+
+Swatches.displayName = '@mantine/core/Swatches';
