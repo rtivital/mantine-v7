214d48aa2ca08d5594bc61cf8f1b4aefa8660b77
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-08 20:34:11 +0400

    Update code highlight clipboard icon

--- src/mantine-code-highlight/src/CopyIcon.tsx
+++ src/mantine-code-highlight/src/CopyIcon.tsx
@@ -1,32 +1,40 @@
+import { rem } from '@mantine/core';
 import React from 'react';
 
-interface CopyIconProps {
+interface CopyIconProps extends React.ComponentPropsWithoutRef<'svg'> {
   copied: boolean;
 }
 
-export function CopyIcon({ copied }: CopyIconProps) {
+export function CopyIcon({ copied, style, ...others }: CopyIconProps) {
   return (
     <svg
-      width="1rem"
-      height="1rem"
-      viewBox="0 0 15 15"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      style={{ width: rem(18), height: rem(18), ...style }}
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...others}
     >
       {copied ? (
-        <path
-          d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
-          fill="currentColor"
-          fillRule="evenodd"
-          clipRule="evenodd"
-        />
+        <>
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+          <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+          <path d="M9 14l2 2l4 -4" />
+        </>
       ) : (
-        <path
-          d="M5 2V1H10V2H5ZM4.75 0C4.33579 0 4 0.335786 4 0.75V1H3.5C2.67157 1 2 1.67157 2 2.5V12.5C2 13.3284 2.67157 14 3.5 14H11.5C12.3284 14 13 13.3284 13 12.5V2.5C13 1.67157 12.3284 1 11.5 1H11V0.75C11 0.335786 10.6642 0 10.25 0H4.75ZM11 2V2.25C11 2.66421 10.6642 3 10.25 3H4.75C4.33579 3 4 2.66421 4 2.25V2H3.5C3.22386 2 3 2.22386 3 2.5V12.5C3 12.7761 3.22386 13 3.5 13H11.5C11.7761 13 12 12.7761 12 12.5V2.5C12 2.22386 11.7761 2 11.5 2H11Z"
-          fill="currentColor"
-          fillRule="evenodd"
-          clipRule="evenodd"
-        />
+        <>
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+          <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+          <path d="M9 12l.01 0" />
+          <path d="M13 12l2 0" />
+          <path d="M9 16l.01 0" />
+          <path d="M13 16l2 0" />
+        </>
       )}
     </svg>
   );
