e3604ef9fa94bbafd8bbaeb3497cfa5c91c3e88c
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-08 11:51:27 +0400

    Fix incorrect slider marks styles

--- src/mantine-core/src/components/Slider/Slider.story.tsx
+++ src/mantine-core/src/components/Slider/Slider.story.tsx
@@ -3,14 +3,20 @@ import { Slider } from './Slider/Slider';
 
 export default { title: 'Slider' };
 
+const marks = [
+  { value: 20, label: '20%' },
+  { value: 50, label: '50%' },
+  { value: 80, label: '80%' },
+];
+
 export function Usage() {
   return (
     <div style={{ padding: 40, maxWidth: 400 }}>
-      <Slider defaultValue={45} />
-      <Slider defaultValue={45} disabled mt="xl" />
+      <Slider defaultValue={45} marks={marks} size="md" />
+      <Slider defaultValue={45} disabled mt={60} />
       <Slider defaultValue={45} inverted mt="xl" />
       <Slider defaultValue={45} inverted disabled mt="xl" />
-      <Slider defaultValue={45} radius={0} mt="xl" />
+      <Slider defaultValue={45} radius={0} mt="xl" labelAlwaysOn />
     </div>
   );
 }
