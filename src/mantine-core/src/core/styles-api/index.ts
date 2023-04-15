cc91f1cb38c0b573b681180c8545058cbcdc05ce
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-15 19:35:48 +0400

    Remove old useVars hook

--- src/mantine-core/src/core/styles-api/index.ts
+++ src/mantine-core/src/core/styles-api/index.ts
@@ -1,4 +1,3 @@
-export { useVars } from './use-vars/use-vars';
 export { createVarsResolver } from './create-vars-resolver/create-vars-resolver';
 export * from './styles-api.types';
 export * from './use-styles/use-styles';
