6756638a98c4833a3c371724948b17bb23501f11
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-16 17:17:14 +0400

    init basic navbar and header docs elements

--- docs/components/Shell/Header/Header.tsx
+++ docs/components/Shell/Header/Header.tsx
@@ -1,4 +1,9 @@
+/* eslint-disable import/no-relative-packages */
 import React from 'react';
+import Link from 'next/link';
+import { MantineLogo } from '@mantine/logo';
+import { Code } from '@mantine/core';
+import packageJson from '../../../../package.json';
 import classes from './Header.module.css';
 
 interface HeaderProps {
@@ -7,5 +12,16 @@ interface HeaderProps {
 }
 
 export function Header({ navbarOpened, onNavbarToggle }: HeaderProps) {
-  return <header className={classes.header}>This is header</header>;
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/" className={classes.logoLink}>
+          <MantineLogo size={30}></MantineLogo>
+        </Link>
+        <Code fw="bold" className={classes.version}>
+          {packageJson.version}
+        </Code>
+      </div>
+    </header>
+  );
 }
--- docs/components/Shell/Navbar/Navbar.tsx
+++ docs/components/Shell/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, ScrollArea } from '@mantine/core';
 import classes from './Navbar.module.css';
 
 interface NavbarProps {
@@ -6,6 +7,16 @@ interface NavbarProps {
   onNavbarClose(): void;
 }
 
+const content = Array(100)
+  .fill(0)
+  .map((_, index) => <p>Content</p>);
+
 export function Navbar({ navbarOpened, onNavbarClose }: NavbarProps) {
-  return <nav className={classes.navbar}>Navabr</nav>;
+  return (
+    <Box component="nav" className={classes.navbar} mod={{ hidden: !navbarOpened }}>
+      <ScrollArea className={classes.scrollarea} type="never" offsetScrollbars={false}>
+        <div className={classes.content}>{content}</div>
+      </ScrollArea>
+    </Box>
+  );
 }
