6411e0128df1e3c91c78eef31d79a0a8047355f0
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-16 09:56:10 +0400

    Cleanup table components types

--- src/mantine-core/src/components/Table/Table.components.tsx
+++ src/mantine-core/src/components/Table/Table.components.tsx
@@ -1,68 +1,48 @@
 import React from 'react';
-import {
-  Box,
-  BoxProps,
-  StylesApiProps,
-  ElementProps,
-  factory,
-  useProps,
-  FactoryPayload,
-} from '../../core';
+import { Box, BoxProps, ElementProps, factory, useProps, FactoryPayload } from '../../core';
 import { useTableContext, TableContextValue } from './Table.context';
-import type { TableFactory } from './Table';
 
-export interface TableElementProps<Selector extends string>
-  extends BoxProps,
-    StylesApiProps<Omit<TableFactory, 'stylesNames'> & { stylesNames: Selector }> {}
-
-export interface TableThProps extends TableElementProps<'th'>, ElementProps<'th'> {}
-export interface TableTdProps extends TableElementProps<'td'>, ElementProps<'td'> {}
-export interface TableTrProps extends TableElementProps<'tr'>, ElementProps<'tr'> {}
-export interface TableTheadProps extends TableElementProps<'thead'>, ElementProps<'thead'> {}
-export interface TableTbodyProps extends TableElementProps<'tbody'>, ElementProps<'tbody'> {}
-export interface TableTfootProps extends TableElementProps<'tfoot'>, ElementProps<'tfoot'> {}
-export interface TableCaptionProps extends TableElementProps<'caption'>, ElementProps<'caption'> {}
+export interface TableThProps extends BoxProps, ElementProps<'th'> {}
+export interface TableTdProps extends BoxProps, ElementProps<'td'> {}
+export interface TableTrProps extends BoxProps, ElementProps<'tr'> {}
+export interface TableTheadProps extends BoxProps, ElementProps<'thead'> {}
+export interface TableTbodyProps extends BoxProps, ElementProps<'tbody'> {}
+export interface TableTfootProps extends BoxProps, ElementProps<'tfoot'> {}
+export interface TableCaptionProps extends BoxProps, ElementProps<'caption'> {}
 
 export interface TableThFactory {
   props: TableThProps;
   ref: HTMLTableCellElement;
-  stylesNames: 'th';
 }
 
 export interface TableTdFactory {
   props: TableTdProps;
   ref: HTMLTableCellElement;
-  stylesNames: 'td';
 }
 
 export interface TableTrFactory {
   props: TableTrProps;
   ref: HTMLTableRowElement;
-  stylesNames: 'tr';
 }
 
 export interface TableTheadFactory {
   props: TableTheadProps;
   ref: HTMLTableSectionElement;
-  stylesNames: 'thead';
 }
 
 export interface TableTbodyFactory {
   props: TableTbodyProps;
   ref: HTMLTableSectionElement;
-  stylesNames: 'tbody';
 }
 
 export interface TableTfootFactory {
   props: TableTfootProps;
   ref: HTMLTableSectionElement;
-  stylesNames: 'tfoot';
 }
 
 export interface TableCaptionFactory {
   props: TableCaptionProps;
   ref: HTMLTableCaptionElement;
-  stylesNames: 'caption';
 }
 
 interface TableElementOptions {
