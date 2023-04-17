eb0a9260b68c8f150d8e6eae0c9b8c3a931e40ea
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-17 12:38:10 +0400

    init docs footer

---
+++ docs/components/Footer/Footer.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Container, Text, Group } from '@mantine/core';
+import { TwitterButton, DiscordButton } from '@mantine/ds';
+import { Logo } from '../Logo/Logo';
+import { LinksGroup } from './LinksGroup/LinksGroup';
+import { FOOTER_LINKS_DATA } from './data';
+import classes from './Footer.module.css';
+
+interface FooterProps {
+  withNavbar?: boolean;
+}
+
+export function Footer({ withNavbar }: FooterProps) {
+  const groups = FOOTER_LINKS_DATA.map((group) => (
+    <LinksGroup data={group.data} title={group.title} key={group.title} />
+  ));
+
+  return (
+    <>
+      <div className={classes.spacer} />
+      <div className={cx(classes.wrapper, { [classes.withNavbar]: withNavbar })}>
+        <Container size={1100}>
+          <div className={classes.inner}>
+            <div className={classes.logoSection}>
+              <Logo />
+              <Text className={classes.description} fz="sm">
+                Build fully functional accessible web applications faster than ever
+              </Text>
+            </div>
+
+            <div className={classes.groups}>{groups}</div>
+          </div>
+
+          <div className={classes.afterFooter}>
+            <Group justify="space-between">
+              <Text fz="xs" className={classes.afterFooterNote}>
+                Built by <a href="https://github.com/rtivital">Vitaly Rtishchev</a> and{' '}
+                <a href="https://github.com/mantinedev/mantine/graphs/contributors">
+                  these awesome people
+                </a>
+              </Text>
+              <div className={classes.social}>
+                <DiscordButton className={classes.socialButton} />
+                <TwitterButton className={classes.socialButton} ml="md" />
+              </div>
+            </Group>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
