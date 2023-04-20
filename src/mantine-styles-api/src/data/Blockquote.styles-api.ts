2657616998726fb34089ebfe4823aa7e3a76d458
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-20 17:04:14 +0400

    Add blockquote styles api

---
+++ src/mantine-styles-api/src/data/Blockquote.styles-api.ts
@@ -0,0 +1,19 @@
+import type { BlockquoteFactory } from '@mantine/core';
+import type { StylesApiData } from '../types';
+
+export const BlockquoteStylesApi: StylesApiData<BlockquoteFactory> = {
+  selectors: {
+    root: 'Root element',
+    icon: 'Icon element',
+  },
+
+  vars: {
+    root: {
+      '--bq-bd': 'Controls border color',
+      '--bq-bg-dark': 'Controls background color in dark color scheme',
+      '--bq-bg-light': 'Controls background color in light color scheme',
+      '--bq-icon-size': 'Controls width and height of icon',
+      '--bq-radius': 'Controls border-radius',
+    },
+  },
+};
