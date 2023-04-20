e94ee1aee2f6be816f20ded3adee20926cc2aa10
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-20 20:55:52 +0400

    Add responsive styles to header

--- docs/components/Shell/Header/Header.tsx
+++ docs/components/Shell/Header/Header.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable import/no-relative-packages */
 import React from 'react';
-import Link from 'next/link';
-import { Code } from '@mantine/core';
-import { HeaderControls } from '@mantine/ds';
+import { Code, Group } from '@mantine/core';
+import { HeaderControls, ColorSchemeControl } from '@mantine/ds';
 import { Logo } from '@/components/Logo';
 import packageJson from '../../../../package.json';
 import classes from './Header.module.css';
@@ -14,15 +13,30 @@ interface HeaderProps {
 
 export function Header({ navbarOpened, onNavbarToggle }: HeaderProps) {
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>
-        <Logo />
-        <Code fw="bold" className={classes.version}>
-          {packageJson.version}
-        </Code>
-      </div>
+    <>
+      <header className={classes.header} data-desktop>
+        <div className={classes.logo}>
+          <Logo />
+          <Code fw="bold" className={classes.version}>
+            {packageJson.version}
+          </Code>
+        </div>
 
-      <HeaderControls onSearch={() => {}} githubLink="https://github.com/mantinedev/mantine" />
-    </header>
+        <HeaderControls
+          className={classes.controls}
+          onSearch={() => {}}
+          githubLink="https://github.com/mantinedev/mantine"
+        />
+      </header>
+
+      <header className={classes.header} data-mobile>
+        <Group justify="space-between" w="100%">
+          <div>bur</div>
+          <Logo />
+
+          <ColorSchemeControl />
+        </Group>
+      </header>
+    </>
   );
 }
