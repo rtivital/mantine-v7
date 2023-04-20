e9f5c97ba1fea16c11b956da6acd658e47567cd2
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-20 10:28:32 +0400

    Add data table and next link components to mdx provider

---
+++ docs/components/MdxProvider/DataTable/DataTable.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Table, rem } from '@mantine/core';
+import { DocsSection } from '@/components/DocsSection';
+
+interface DataTableProps {
+  data: React.ReactNode[][];
+  head?: string[];
+}
+
+export function DataTable({ data, head }: DataTableProps) {
+  const rows = data.map((row, index) => {
+    const cells = row.map((cell, cellIndex) => <Table.Td key={cellIndex}>{cell}</Table.Td>);
+    return <Table.Tr key={index}>{cells}</Table.Tr>;
+  });
+
+  const ths = Array.isArray(head)
+    ? head.map((cell, index) => <Table.Th key={index}>{cell}</Table.Th>)
+    : null;
+
+  return (
+    <DocsSection>
+      <div style={{ overflowX: 'auto' }}>
+        <div style={{ minWidth: rem(400) }}>
+          <Table>
+            {ths && (
+              <Table.Thead>
+                <Table.Tr>{ths}</Table.Tr>
+              </Table.Thead>
+            )}
+            <Table.Tbody>{rows}</Table.Tbody>
+          </Table>
+        </div>
+      </div>
+    </DocsSection>
+  );
+}
--- docs/components/MdxProvider/MdxProvider.tsx
+++ docs/components/MdxProvider/MdxProvider.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { CodeHighlight } from '@mantine/code-highlight';
 import { Demo } from '@mantine/ds';
 import { MDXProvider } from '@mdx-js/react';
+import { NextLink } from './NextLink/NextLink';
+import { DataTable } from './DataTable/DataTable';
 
 export function MdxProvider({ children }: { children: React.ReactNode }) {
   return (
     <MDXProvider
       components={{
         Demo,
+        NextLink,
+        DataTable,
         pre: (props: any) => {
           const matches = (props.children.props.className || '').match(/language-(?<lang>.*)/);
           return (
