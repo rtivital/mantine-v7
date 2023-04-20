b073d1bf3d9bad36dda0e473a1336135db286327
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-20 12:27:59 +0400

    Add mdx title

--- docs/components/MdxProvider/MdxProvider.tsx
+++ docs/components/MdxProvider/MdxProvider.tsx
@@ -4,6 +4,7 @@ import { MDXProvider } from '@mdx-js/react';
 import { NextLink } from './NextLink/NextLink';
 import { DataTable } from './DataTable/DataTable';
 import { Pre } from './Pre/Pre';
+import { h } from './MdxTitle/MdxTitle';
 
 export function MdxProvider({ children }: { children: React.ReactNode }) {
   return (
@@ -13,6 +14,12 @@ export function MdxProvider({ children }: { children: React.ReactNode }) {
         NextLink,
         DataTable,
         pre: Pre,
+        h1: h(1),
+        h2: h(2),
+        h3: h(3),
+        h4: h(4),
+        h5: h(5),
+        h6: h(6),
       }}
     >
       {children}
---
+++ docs/components/MdxProvider/MdxTitle/MdxTitle.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import cx from 'clsx';
+import { Title, rem, useMantineTheme } from '@mantine/core';
+import classes from './MdxTitle.module.css';
+
+export default function MdxTitle({
+  id,
+  children,
+  order,
+  ...others
+}: React.ComponentPropsWithoutRef<typeof Title>) {
+  const theme = useMantineTheme();
+
+  if (order === 1) {
+    return (
+      <Title className={classes.title} style={{ fontWeight: 900, fontSize: rem(44) }}>
+        {children}
+      </Title>
+    );
+  }
+
+  return (
+    <>
+      <div id={id} className={classes.offset} />
+      <Title order={order} className={classes.title} style={{ fontWeight: 600 }} {...others}>
+        <a className={cx(classes.link, theme.focusClassNames.auto)} href={`#${id}`}>
+          {children}
+        </a>
+      </Title>
+    </>
+  );
+}
+
+export const h = (order: 1 | 2 | 3 | 4 | 5 | 6) => (props: any) =>
+  <MdxTitle order={order} {...props} />;
