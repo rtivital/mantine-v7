911cd951a392ddd8aa77bfc496ae1fa0461a239b
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-23 16:30:05 +0400

    Add activeClassName documnetation

--- src/mantine-demos/src/demos/theming/index.ts
+++ src/mantine-demos/src/demos/theming/index.ts
@@ -1,2 +1,4 @@
 export { focusRing } from './Theming.demo.focusRing';
 export { focusClassName } from './Theming.demo.focusClassName';
+export { activeClassName } from './Theming.demo.activeClassName';
+export { activeClassNameEmpty } from './Theming.demo.activeClassNameEmpty';
