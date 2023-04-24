c42823890be424999963a0493fb43ae5afc05645
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-24 15:34:16 +0400

    Init colors generator

--- docs/components/ColorsGenerator/ColorsGenerator.tsx
+++ docs/components/ColorsGenerator/ColorsGenerator.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { ColorSwatch, Group, Text, DEFAULT_THEME } from '@mantine/core';
+import { generateColors } from './generate-colors/generate-colors';
 
 export function ColorsGenerator() {
-  return <div>Colors</div>;
+  const { colors, hueDown, hueUp, baseColorIndex } = generateColors('#FFE3E3');
+
+  console.log(baseColorIndex);
+
+  const swatches = colors.map((color, index) => (
+    <div key={color.hex()}>
+      <ColorSwatch size={80} color={color.hex()} />
+      <Text fz="xs">
+        {index} – {color.hex()}
+      </Text>
+    </div>
+  ));
+
+  const themeColors = DEFAULT_THEME.colors.red.map((color, index) => (
+    <div key={color}>
+      <ColorSwatch size={80} color={color} />
+      <Text fz="xs">
+        {index} – {color}
+      </Text>
+    </div>
+  ));
+
+  return (
+    <div>
+      <Group>{swatches}</Group>
+      <Group>{themeColors}</Group>
+    </div>
+  );
 }
