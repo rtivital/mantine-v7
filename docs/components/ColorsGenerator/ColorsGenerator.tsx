72b3f087be784e602633eb523f034a39f6b8762f
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-24 13:35:20 +0400

    Init colors generator page

---
+++ docs/components/ColorsGenerator/ColorsGenerator.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function ColorsGenerator() {
+  return <div>Colors</div>;
+}
---
+++ docs/pages/colors-generator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Shell } from '@/components/Shell';
+import { ColorsGenerator } from '@/components/ColorsGenerator';
+
+export default function ColorsGeneratorPage() {
+  return (
+    <Shell>
+      <ColorsGenerator />
+    </Shell>
+  );
+}
