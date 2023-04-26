fd014b50c2fcf524b14593a22f63b40d2c307d57
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-26 17:42:47 +0400

    Update copy icon in code highlight

--- src/mantine-code-highlight/src/CopyIcon.tsx
+++ src/mantine-code-highlight/src/CopyIcon.tsx
@@ -20,20 +20,14 @@ export function CopyIcon({ copied, style, ...others }: CopyIconProps) {
     >
       {copied ? (
         <>
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
-          <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
-          <path d="M9 14l2 2l4 -4" />
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M5 12l5 5l10 -10"></path>
         </>
       ) : (
         <>
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
-          <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
-          <path d="M9 12l.01 0" />
-          <path d="M13 12l2 0" />
-          <path d="M9 16l.01 0" />
-          <path d="M13 16l2 0" />
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+          <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
         </>
       )}
     </svg>
