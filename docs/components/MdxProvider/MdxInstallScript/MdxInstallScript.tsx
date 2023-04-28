485431a55df7ead7744dbc3d524d583bfb5795b7
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-28 17:51:39 +0400

    Add postcss preset documnetation

--- docs/components/MdxProvider/MdxInstallScript/MdxInstallScript.tsx
+++ docs/components/MdxProvider/MdxInstallScript/MdxInstallScript.tsx
@@ -5,22 +5,23 @@ import classes from './MdxInstallScript.module.css';
 
 interface MdxInstallScriptProps {
   packages: string;
+  dev?: boolean;
 }
 
-export function MdxInstallScript({ packages }: MdxInstallScriptProps) {
+export function MdxInstallScript({ packages, dev }: MdxInstallScriptProps) {
   return (
     <CodeHighlightTabs
       classNames={{ root: classes.root }}
       code={[
         {
           fileName: 'yarn',
-          code: `yarn add ${packages}`,
+          code: `yarn add ${dev ? '--dev ' : ''}${packages}`,
           language: 'bash',
           icon: <YarnIcon className={classes.icon} size={16} />,
         },
         {
           fileName: 'npm',
-          code: `npm install ${packages}`,
+          code: `npm install ${dev ? '--save-dev ' : ''}${packages}`,
           language: 'bash',
           icon: <NpmIcon className={classes.icon} size={16} />,
         },
