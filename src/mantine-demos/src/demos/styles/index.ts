7e666c39d00a46d28acfc8ead7a12cb7017961b1
Vitaly Rtishchev <rtivital@gmail.com>
2023-04-28 14:42:47 +0400

    Init css modules documnetation

---
+++ src/mantine-demos/src/demos/styles/index.ts
@@ -0,0 +1,1 @@
+export { className } from './Styles.demo.className';
--- src/mantine-demos/src/index.ts
+++ src/mantine-demos/src/index.ts
@@ -1,3 +1,4 @@
 export * as ThemingDemos from './demos/theming';
+export * as StylesDemos from './demos/styles';
 
 export * as LoaderDemos from './demos/core/Loader';
