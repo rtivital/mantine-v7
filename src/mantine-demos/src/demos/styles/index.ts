0eab58d6192ff8fdabd6bb9799dc216f9a1abaa7
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-01 12:40:27 +0400

    Add responsive styles documentation

--- src/mantine-demos/src/demos/styles/index.ts
+++ src/mantine-demos/src/demos/styles/index.ts
@@ -1,2 +1,5 @@
 export { className } from './Styles.demo.className';
 export { globalClasses } from './Styles.demo.globalClasses';
+export { responsive } from './Styles.demo.responsive';
+export { sizesMedia } from './Styles.demo.sizesMedia';
+export { useMediaQueryHook } from './Styles.demo.useMediaQueryHook';
