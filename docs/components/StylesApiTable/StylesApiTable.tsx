2d72eabc6261e18158c83c37bd76d3759c0b55a9
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-04 10:13:07 +0400

    Split styles api tables into multiple components

--- docs/components/StylesApiTable/StylesApiTable.tsx
+++ docs/components/StylesApiTable/StylesApiTable.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
-import { Text, Anchor, Table, rem } from '@mantine/core';
+import { Text, Anchor, rem } from '@mantine/core';
 import * as stylesData from '@mantine/styles-api';
 import type { Modifier } from '@mantine/styles-api';
-import { HtmlText } from '@/components/HtmlText';
-import { TableInlineCode } from '@/components/TableInlineCode';
 import { TableError } from '@/components/TableError';
+import { SelectorsTable } from './SelectorsTable';
+import { VariablesTable } from './VariablesTable';
+import { ModifiersTable } from './ModifiersTable';
 import classes from './StylesApiTable.module.css';
 
 export interface StylesApiData {
@@ -26,58 +27,6 @@ export function StylesApiTable({ component }: StylesApiTableProps) {
     return <TableError errorOf="Styles API" />;
   }
 
-  const selectorsRows = Object.keys(data.selectors).map((selector) => (
-    <Table.Tr key={selector}>
-      <Table.Td>{selector}</Table.Td>
-      <Table.Td>
-        <TableInlineCode>
-          .mantine-{component}-{selector}
-        </TableInlineCode>
-      </Table.Td>
-      <Table.Td>
-        <HtmlText>{data.selectors[selector]}</HtmlText>
-      </Table.Td>
-    </Table.Tr>
-  ));
-
-  const variablesRows = Object.keys(data.vars).reduce<React.ReactNode[]>((acc, selector) => {
-    Object.keys(data.vars[selector]).forEach((variable, index) => {
-      acc.push(
-        <Table.Tr key={`${selector}-${variable}`}>
-          {index === 0 && (
-            <Table.Td rowSpan={Object.keys(data.vars[selector]).length}>{selector}</Table.Td>
-          )}
-          <Table.Td>
-            <TableInlineCode>{variable}</TableInlineCode>
-          </Table.Td>
-          <Table.Td>
-            <HtmlText>{data.vars[selector][variable]}</HtmlText>
-          </Table.Td>
-        </Table.Tr>
-      );
-    });
-
-    return acc;
-  }, []);
-
-  const modifiersRows =
-    data.modifiers?.map((modifier, index) => (
-      <Table.Tr key={index}>
-        <Table.Td>
-          {Array.isArray(modifier.selector) ? modifier.selector.join(', ') : modifier.selector}
-        </Table.Td>
-        <Table.Td>
-          <TableInlineCode>{modifier.modifier}</TableInlineCode>
-        </Table.Td>
-        <Table.Td>
-          <HtmlText>{modifier.condition || '–'}</HtmlText>
-        </Table.Td>
-        <Table.Td>
-          <HtmlText>{modifier.value || '–'}</HtmlText>
-        </Table.Td>
-      </Table.Tr>
-    )) || [];
-
   return (
     <>
       <div style={{ overflowX: 'auto' }}>
@@ -87,54 +36,26 @@ export function StylesApiTable({ component }: StylesApiTableProps) {
             <Anchor component={Link} href="/styles/styles-api/" className={classes.link}>
               Learn how to customize components styles with Styles API →
             </Anchor>
-            <Table withTableBorder>
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th>Selector</Table.Th>
-                  <Table.Th>Static selector</Table.Th>
-                  <Table.Th>Description</Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody>{selectorsRows}</Table.Tbody>
-            </Table>
+            <SelectorsTable component={component} data={data} />
           </div>
 
-          {variablesRows.length > 0 && (
+          {Object.keys(data.vars).length > 0 && (
             <div className={classes.section}>
               <Text className={classes.title}>{component} CSS variables</Text>
               <Anchor component={Link} href="/styles/styles-api/" className={classes.link}>
                 Learn how to handle dynamic styles with CSS variables →
               </Anchor>
-              <Table withTableBorder>
-                <Table.Thead>
-                  <Table.Tr>
-                    <Table.Th>Selector</Table.Th>
-                    <Table.Th>Variable</Table.Th>
-                    <Table.Th>Description</Table.Th>
-                  </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>{variablesRows}</Table.Tbody>
-              </Table>
+              <VariablesTable data={data} />
             </div>
           )}
 
-          {modifiersRows.length > 0 && (
+          {Array.isArray(data.modifiers) && data.modifiers.length > 0 && (
             <div className={classes.section}>
               <Text className={classes.title}>{component} component modifiers</Text>
               <Anchor component={Link} href="/styles/styles-api/" className={classes.link}>
                 Learn how to use data attributes to customize styles →
               </Anchor>
-              <Table withTableBorder>
-                <Table.Thead>
-                  <Table.Tr>
-                    <Table.Th>Selector</Table.Th>
-                    <Table.Th>Modifier</Table.Th>
-                    <Table.Th>Condition</Table.Th>
-                    <Table.Th>Value</Table.Th>
-                  </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>{modifiersRows}</Table.Tbody>
-              </Table>
+              <ModifiersTable data={data} />
             </div>
           )}
         </div>
--- docs/components/StylesApiTable/index.ts
+++ docs/components/StylesApiTable/index.ts
@@ -1,2 +1,4 @@
 export { StylesApiTable } from './StylesApiTable';
 export { ModifiersTable } from './ModifiersTable';
+export { SelectorsTable } from './SelectorsTable';
+export { VariablesTable } from './VariablesTable';
