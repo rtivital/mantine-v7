b69c1894361ee2f0f7d0b508ccf4ad523310954b
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-07 18:50:10 +0400

    Add Input demos

---
+++ src/mantine-demos/src/demos/core/Input/Input.demo.defaultProps.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { MantineDemo } from '@mantine/ds';
+import { TextInput, NativeSelect, MantineThemeProvider, createTheme, Input } from '@mantine/core';
+
+const code = `
+`;
+
+const theme = createTheme({
+  components: {
+    Input: Input.extend({
+      defaultProps: {
+        variant: 'filled',
+      },
+    }),
+
+    InputWrapper: Input.Wrapper.extend({
+      defaultProps: {
+        inputWrapperOrder: ['label', 'input', 'description', 'error'],
+      },
+    }),
+  },
+});
+
+function Demo() {
+  return (
+    <MantineThemeProvider theme={theme}>
+      <TextInput
+        label="Text input"
+        placeholder="Text input"
+        description="Description below the input"
+      />
+
+      <NativeSelect
+        mt="md"
+        label="Native select"
+        placeholder="Native select"
+        data={['React', 'Angular', 'Vue', 'Svelte']}
+        description="Description below the input"
+      />
+    </MantineThemeProvider>
+  );
+}
+
+export const defaultProps: MantineDemo = {
+  type: 'code',
+  component: Demo,
+  centered: true,
+  maxWidth: 340,
+  code,
+};
--- src/mantine-demos/src/shared/input-controls.ts
+++ src/mantine-demos/src/shared/input-controls.ts
@@ -13,3 +13,11 @@ export const inputOnlyControls: ConfiguratorControlOptions[] = [
   { type: 'boolean', prop: 'disabled', initialValue: false, libraryValue: false },
   { type: 'boolean', prop: 'error', initialValue: false, libraryValue: false },
 ];
+
+export const inputWrapperOnlyControls: ConfiguratorControlOptions[] = [
+  { type: 'string', prop: 'label', initialValue: 'Input label', libraryValue: null },
+  { type: 'boolean', prop: 'withAsterisk', initialValue: false, libraryValue: false },
+  { type: 'string', prop: 'description', initialValue: 'Input description', libraryValue: null },
+  { type: 'string', prop: 'error', initialValue: 'Input error', libraryValue: null },
+  { type: 'size', prop: 'size', initialValue: 'sm', libraryValue: 'sm' },
+];
