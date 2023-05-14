e7340ccb899521f862bf985b76770b2cef54b207
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-14 11:50:24 +0400

    Remove emotion from build scripts

--- configuration/rollup/create-package-config.ts
+++ configuration/rollup/create-package-config.ts
@@ -65,12 +65,8 @@ export default async function createPackageConfig(config: PkgConfigInput): Promi
     ];
   } else {
     externals = [
-      '@emotion/server/create-instance',
       'dayjs/locale/ru',
       'highlight.js/lib/languages/typescript',
-      '@emotion/cache',
-      '@emotion/utils',
-      '@emotion/serialize',
       'prism-react-renderer/themes/duotoneDark',
       'prism-react-renderer/themes/duotoneLight',
       ...(config?.externals || []),
