e00a0a31f72a1a90ecad62592729885d5bf5c3b3
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-15 11:02:36 +0400

    Move Alert  configurator demo to stipped

--- src/mantine-demos/src/demos/core/Alert/Alert.demo.configurator.tsx
+++ src/mantine-demos/src/demos/core/Alert/Alert.demo.configurator.tsx
@@ -23,6 +23,7 @@ export const configurator: MantineDemo = {
   component: Wrapper,
   code,
   centered: true,
+  striped: true,
   maxWidth: 400,
   controls: [
     { ...(staticVariantsControl as any), initialValue: 'light' },
