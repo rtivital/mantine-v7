85716dd4b2a190cbc7968f5a7bf328563123ebad
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-17 22:52:45 +0400

    Cleanup CopyButton docs

--- src/mantine-core/src/components/CopyButton/CopyButton.tsx
+++ src/mantine-core/src/components/CopyButton/CopyButton.tsx
@@ -7,7 +7,7 @@ export interface CopyButtonProps {
   /** Children callback, provides current status and copy function as an argument */
   children(payload: { copied: boolean; copy(): void }): React.ReactNode;
 
-  /** Value that will be copied to the clipboard when button is clicked */
+  /** Value that will be copied to the clipboard when the button is clicked */
   value: string;
 
   /** Copied status timeout in ms, `1000` by default */
