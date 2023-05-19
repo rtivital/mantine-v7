d3978732476ea2c28aca998727d6fe3f12eb78a2
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-19 17:38:13 +0400

    Remove transitions from all elements on color scheme switch

--- src/mantine-core/src/core/MantineProvider/use-mantine-color-scheme/use-mantine-color-scheme.ts
+++ src/mantine-core/src/core/MantineProvider/use-mantine-color-scheme/use-mantine-color-scheme.ts
@@ -1,16 +1,59 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { MantineContext } from '../Mantine.context';
+import { MantineColorScheme } from '../theme.types';
+
+function disableTransition() {
+  const style = document.createElement('style');
+  style.innerHTML = '* {transition: none !important;}';
+  document.head.appendChild(style);
+  const clear = () => {
+    try {
+      document.head.removeChild(style);
+    } catch (e) {
+      // Already removed
+    }
+  };
+  return clear;
+}
 
 export function useMantineColorScheme() {
+  const clearStylesRef = useRef<() => void>();
+  const timeoutRef = useRef<number>();
   const ctx = useContext(MantineContext);
 
   if (!ctx) {
     throw new Error('[@mantine/core] MantineProvider was not found in tree');
   }
 
+  const setColorScheme = (value: MantineColorScheme) => {
+    ctx.setColorScheme(value);
+    clearStylesRef.current = disableTransition();
+    window.clearTimeout(timeoutRef.current);
+    timeoutRef.current = window.setTimeout(() => {
+      clearStylesRef.current?.();
+    }, 10);
+  };
+
+  const clearColorScheme = () => {
+    ctx.clearColorScheme();
+    clearStylesRef.current = disableTransition();
+    window.clearTimeout(timeoutRef.current);
+    timeoutRef.current = window.setTimeout(() => {
+      clearStylesRef.current?.();
+    }, 10);
+  };
+
+  useEffect(
+    () => () => {
+      clearStylesRef.current?.();
+      window.clearTimeout(timeoutRef.current);
+    },
+    []
+  );
+
   return {
     colorScheme: ctx.colorScheme,
-    setColorScheme: ctx.setColorScheme,
-    clearColorScheme: ctx.clearColorScheme,
+    setColorScheme,
+    clearColorScheme,
   };
 }
