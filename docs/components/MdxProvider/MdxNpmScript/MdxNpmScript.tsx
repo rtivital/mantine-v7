b2d5104dc7dbe4d55d154b40dfc26dbdcff22a15
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-25 17:00:11 +0400

    Sync npm/yarn script with local storage

--- docs/components/MdxProvider/MdxNpmScript/MdxNpmScript.tsx
+++ docs/components/MdxProvider/MdxNpmScript/MdxNpmScript.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocalStorage } from '@mantine/hooks';
 import { CodeHighlightTabs } from '@mantine/code-highlight';
 import { YarnIcon, NpmIcon } from '@mantine/ds';
 import classes from './MdxNpmScript.module.css';
@@ -9,9 +10,16 @@ interface MdxNpmScriptProps {
 }
 
 export function MdxNpmScript({ yarnScript, npmScript }: MdxNpmScriptProps) {
+  const [tab, setTab] = useLocalStorage({
+    key: 'script-tab',
+    defaultValue: 0,
+  });
+
   return (
     <CodeHighlightTabs
       classNames={{ root: classes.root }}
+      activeTab={tab}
+      onTabChange={setTab}
       code={[
         {
           fileName: 'yarn',
