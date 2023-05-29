fd675d4ca32d13083527c133bf4ed6dc0085845b
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-29 12:57:27 +0400

    Add all templates to the list

--- docs/components/MdxProvider/MdxTemplatesList/data.ts
+++ docs/components/MdxProvider/MdxTemplatesList/data.ts
@@ -20,6 +20,13 @@ export const TEMPLATES_DATA: Template[] = [
   },
   {
     type: 'next',
+    name: 'next-app-min-template',
+    link: 'https://github.com/mantinedev/next-app-min-template',
+    description:
+      'Next.js template with app router and minimal setup – no additional tools included, only default Next.js configuration',
+  },
+  {
+    type: 'next',
     name: 'next-pages-min-template',
     link: 'https://github.com/mantinedev/next-pages-min-template',
     description:
@@ -32,16 +39,30 @@ export const TEMPLATES_DATA: Template[] = [
     description: 'Vite template with full setup: Jest, Storybook, ESLint',
   },
   {
-    type: 'gatsby',
-    name: 'gatsby-template',
-    link: 'https://github.com/mantinedev/gatsby-template',
-    description: 'Gatsby template with basic setup',
+    type: 'vite',
+    name: 'vite-min-template',
+    link: 'https://github.com/mantinedev/vite-min-template',
+    description:
+      'Vite template with minimal setup – no additional tools included, only default Vite configuration',
   },
   {
     type: 'remix',
     name: 'remix-template',
     link: 'https://github.com/mantinedev/remix-template',
     description: 'Remix template with full setup: Jest, Storybook, ESLint',
+  },
+  {
+    type: 'remix',
+    name: 'remix-min-template',
+    link: 'https://github.com/mantinedev/remix-min-template',
+    description:
+      'Remix template with minimal setup – no additional tools included, only default Remix configuration',
+  },
+  {
+    type: 'gatsby',
+    name: 'gatsby-template',
+    link: 'https://github.com/mantinedev/gatsby-template',
+    description: 'Gatsby template with basic setup',
   },
   {
     type: 'redwood',
