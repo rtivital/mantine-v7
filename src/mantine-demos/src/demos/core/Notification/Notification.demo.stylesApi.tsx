acef843a250f673e62658c5c8cf018cd288952cc
Konstantin Darutkin <spalt08@mail.ru>
2023-06-02 00:18:17 +0900

    docs: add docs pages for Notification

--- src/mantine-demos/src/demos/core/Notification/Notification.demo.stylesApi.tsx
+++ src/mantine-demos/src/demos/core/Notification/Notification.demo.stylesApi.tsx
@@ -5,30 +5,13 @@ import { NotificationStylesApi } from '@mantine/styles-api';
 import { IconCheck } from '@tabler/icons-react';
 
 const code = `
-import { Box, Notification } from '@mantine/core';
+import { Notification } from '@mantine/core';
 
-const icon = <IconCheck size="1.2rem" />;
-
-function Demo(props: any) {
+function Demo() {
   return (
-    <Box maw={400} mx="auto">
-      <Notification
-       {{props}}
-        title="Please wait"
-        loading
-        withCloseButton={false}
-      >
-        The application is trying to reconnect to the server
-      </Notification>
-      <Notification
-       {{props}}
-        icon={icon}
-        title="We notify you that"
-        withBorder
-      >
-        You are now obligated to give a star to Mantine project on GitHub
-      </Notification>
-    </Box>
+    <Notification{{props}} title="We notify you that">
+      You are now obligated to give a star to Mantine project on GitHub
+    </Notification>
   );
 }
 `;
