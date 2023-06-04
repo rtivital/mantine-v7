bc6c4717bf27eec5116c71c38f1d673421a144b1
Vitaly Rtishchev <rtivital@gmail.com>
2023-06-04 15:01:40 +0400

    Add NotificationContainer

---
+++ src/mantine-notifications/src/NotificationContainer.tsx
@@ -0,0 +1,49 @@
+import React, { forwardRef, useRef, useEffect } from 'react';
+import { Notification, NotificationProps } from '@mantine/core';
+import { NotificationData } from './notifications.store';
+import { getAutoClose } from './get-auto-close/get-auto-close';
+
+interface NotificationContainerProps extends NotificationProps {
+  data: NotificationData;
+  onHide(id: string): void;
+  autoClose: number | false;
+}
+
+export const NotificationContainer = forwardRef<HTMLDivElement, NotificationContainerProps>(
+  ({ data, onHide, autoClose }, ref) => {
+    const { autoClose: _autoClose, message, ...notificationProps } = data;
+    const autoCloseDuration = getAutoClose(autoClose, data.autoClose);
+    const autoCloseTimeout = useRef<number>();
+
+    const cancelAutoClose = () => window.clearTimeout(autoCloseTimeout.current);
+
+    const handleHide = () => {
+      onHide(data.id!);
+      cancelAutoClose();
+    };
+
+    const handleAutoClose = () => {
+      if (typeof autoCloseDuration === 'number') {
+        autoCloseTimeout.current = window.setTimeout(handleHide, autoCloseDuration);
+      }
+    };
+
+    useEffect(() => {
+      data.onOpen?.(data);
+      handleAutoClose();
+      return cancelAutoClose;
+    }, []);
+
+    return (
+      <Notification
+        {...notificationProps}
+        onClose={handleHide}
+        ref={ref}
+        onMouseEnter={cancelAutoClose}
+        onMouseLeave={handleAutoClose}
+      />
+    );
+  }
+);
+
+NotificationContainer.displayName = '@mantine/notifications/NotificationContainer';
