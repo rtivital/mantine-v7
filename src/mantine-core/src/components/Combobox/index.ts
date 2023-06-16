0982b6cdbe3f478461b1c2b09b546425ad8a43e3
Vitaly Rtishchev <rtivital@gmail.com>
2023-06-16 21:40:54 +0400

    Add combobox exports

--- src/mantine-core/src/components/Combobox/index.ts
+++ src/mantine-core/src/components/Combobox/index.ts
@@ -1,5 +1,11 @@
 export { getParsedComboboxData } from './get-parsed-combobox-data/get-parsed-combobox-data';
 export { ComboboxChevron } from './ComboboxChevron/ComboboxChevron';
+export { Combobox } from './Combobox';
+export { ComboboxDropdown } from './ComboboxDropdown/ComboboxDropdown';
+export { ComboboxOptions } from './ComboboxOptions/ComboboxOptions';
+export { ComboboxOption } from './ComboboxOption/ComboboxOption';
+export { ComboboxTarget } from './ComboboxTarget/ComboboxTarget';
+export { useCombobox } from './use-combobox';
 
 export type {
   ComboboxItem,
@@ -8,3 +14,9 @@ export type {
   ComboboxItemGroup,
   ComboboxParsedItemGroup,
 } from './Combobox.types';
+export type { ComboboxFactory, ComboboxProps, ComboboxStylesNames } from './Combobox';
+export type { ComboboxDropdownProps } from './ComboboxDropdown/ComboboxDropdown';
+export type { ComboboxOptionsProps } from './ComboboxOptions/ComboboxOptions';
+export type { ComboboxOptionProps } from './ComboboxOption/ComboboxOption';
+export type { ComboboxTargetProps } from './ComboboxTarget/ComboboxTarget';
+export type { ComboboxStore } from './use-combobox';
