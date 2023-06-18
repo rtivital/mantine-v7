849b45f63f2d35fb42f17dd6b892412516c9a63b
Vitaly Rtishchev <rtivital@gmail.com>
2023-06-18 14:26:25 +0400

    Add searchable select combobox demo

--- src/mantine-demos/src/demos/core/Combobox/index.ts
+++ src/mantine-demos/src/demos/core/Combobox/index.ts
@@ -1,3 +1,4 @@
 export { autocomplete } from './Combobox.demo.autocomplete';
 export { hiddenDropdown } from './Combobox.demo.hiddenDropdown';
 export { select } from './Combobox.demo.select';
+export { searchableSelect } from './Combobox.demo.searchableSelect';
