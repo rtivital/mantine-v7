1a745970fad61c8f3aad52d3925ed2a9fc592677
Vitaly Rtishchev <rtivital@gmail.com>
2023-06-18 14:43:32 +0400

    Add button search combobox demo

--- src/mantine-demos/src/demos/core/Combobox/index.ts
+++ src/mantine-demos/src/demos/core/Combobox/index.ts
@@ -3,3 +3,4 @@ export { hiddenDropdown } from './Combobox.demo.hiddenDropdown';
 export { select } from './Combobox.demo.select';
 export { searchableSelect } from './Combobox.demo.searchableSelect';
 export { button } from './Combobox.demo.button';
+export { buttonSearch } from './Combobox.demo.buttonSearch';
