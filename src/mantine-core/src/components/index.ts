4e6477eea50bdee55aa4794cf076bd8781968b24
Vitaly Rtishchev <rtivital@gmail.com>
2023-07-05 14:59:27 +0400

    Add TagsInput exports

--- src/mantine-core/src/components/index.ts
+++ src/mantine-core/src/components/index.ts
@@ -57,6 +57,7 @@ export * from './Stack';
 export * from './Switch';
 export * from './Table';
 export * from './Tabs';
+export * from './TagsInput';
 export * from './Text';
 export * from './TextInput';
 export * from './Title';
