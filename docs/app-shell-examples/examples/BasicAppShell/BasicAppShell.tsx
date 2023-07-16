4eacd076862ea333bfbcace454be83d177c654dd
Vitaly Rtishchev <rtivital@gmail.com>
2023-07-16 15:26:44 +0400

    Init basic app shell example

--- docs/app-shell-examples/examples/BasicAppShell/BasicAppShell.tsx
+++ docs/app-shell-examples/examples/BasicAppShell/BasicAppShell.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Burger, Group } from '@mantine/core';
 
 export function BasicAppShell() {
-  return <div>Basic app shell</div>;
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" />
+          <div>Header</div>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar>Navbar</AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
 }
