2ebaecc8f8005fd9969c4a34331dfef83f99ed5b
Vitaly Rtishchev <rtivital@gmail.com>
2023-07-20 15:41:13 +0400

    Add better components preview to colors generator

--- docs/components/ColorsGenerator/ComponentsPreview/ComponentsPreview.tsx
+++ docs/components/ColorsGenerator/ComponentsPreview/ComponentsPreview.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MantineColorsTuple, MantineProvider, Button, Group } from '@mantine/core';
+import { MantineColorsTuple, MantineProvider, Button, Input, Table } from '@mantine/core';
 import { useDebouncedValue } from '@mantine/hooks';
 
 interface ComponentsPreviewProps {
@@ -8,18 +8,49 @@ interface ComponentsPreviewProps {
 
 export function ComponentsPreview({ colors }: ComponentsPreviewProps) {
   const [debouncedColors] = useDebouncedValue(colors, 100);
+
   return (
     <>
+      <Input.Label display="block" size="md" labelElement="div" mt="xl" mb="sm">
+        Variants preview
+      </Input.Label>
+
       <MantineProvider theme={{ colors: { '__colors-generator__': debouncedColors } }}>
-        <Group mt="xl">
-          <Button color="__colors-generator__">Filled</Button>
-          <Button color="__colors-generator__" variant="light">
-            Light
-          </Button>
-          <Button color="__colors-generator__" variant="outline">
-            Outline
-          </Button>
-        </Group>
+        <Table withTableBorder withColumnBorders>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th ta="center">Filled</Table.Th>
+              <Table.Th ta="center">Light</Table.Th>
+              <Table.Th ta="center">Outline</Table.Th>
+              <Table.Th ta="center">Subtle</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+
+          <Table.Tbody>
+            <Table.Tr>
+              <Table.Td>
+                <Button color="__colors-generator__" fullWidth>
+                  Button
+                </Button>
+              </Table.Td>
+              <Table.Td>
+                <Button color="__colors-generator__" variant="light" fullWidth>
+                  Button
+                </Button>
+              </Table.Td>
+              <Table.Td>
+                <Button color="__colors-generator__" variant="outline" fullWidth>
+                  Button
+                </Button>
+              </Table.Td>
+              <Table.Td>
+                <Button color="__colors-generator__" variant="subtle" fullWidth>
+                  Button
+                </Button>
+              </Table.Td>
+            </Table.Tr>
+          </Table.Tbody>
+        </Table>
       </MantineProvider>
     </>
   );
