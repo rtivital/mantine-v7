e6f7f3c9f4dfee724b1653fa1c8f2f49f4c60462
Vitaly Rtishchev <rtivital@gmail.com>
2023-07-20 15:10:20 +0400

    Add version picker to docs

--- docs/components/Shell/Header/Header.tsx
+++ docs/components/Shell/Header/Header.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-relative-packages */
 import React from 'react';
 import cx from 'clsx';
-import { Code, Group, Burger, RemoveScroll } from '@mantine/core';
+import { IconChevronDown, IconExternalLink } from '@tabler/icons-react';
+import { Code, Group, Burger, RemoveScroll, Menu, UnstyledButton, Text } from '@mantine/core';
 import { HeaderControls, ColorSchemeControl } from '@mantine/ds';
 import { Logo } from '@/components/Logo';
 import { searchHandlers } from '@/components/Search';
@@ -13,15 +14,48 @@ interface HeaderProps {
   onNavbarToggle(): void;
 }
 
+const versions = [
+  { v: 'v6', name: '6.0.20', link: 'https://v6.mantine.dev/' },
+  { v: 'v5', name: '5.10.5', link: 'https://v5.mantine.dev/' },
+  { v: 'v4', name: '4.2.12', link: 'https://v4.mantine.dev/' },
+  { v: 'v3', name: '3.6.14', link: 'https://v3.mantine.dev/' },
+  { v: 'v2', name: '2.5.1', link: 'https://v2.mantine.dev/' },
+  { v: 'v1', name: '1.3.1', link: 'https://v1.mantine.dev/' },
+];
+
 export function Header({ navbarOpened, onNavbarToggle }: HeaderProps) {
+  const versionItems = versions.map((item) => (
+    <Menu.Item
+      key={item.name}
+      component="a"
+      href={item.link}
+      target="_blank"
+      rightSection={<IconExternalLink className={classes.versionExternalIcon} stroke={1.5} />}
+    >
+      <b>{item.v}</b>{' '}
+      <Text span c="dimmed" fz="xs">
+        ({item.name})
+      </Text>
+    </Menu.Item>
+  ));
+
   return (
     <>
       <header className={cx(classes.header, RemoveScroll.classNames.fullWidth)} data-desktop>
         <div className={classes.logo}>
           <Logo />
-          <Code fw="bold" className={classes.version}>
-            {packageJson.version}
-          </Code>
+          <Menu width={180}>
+            <Menu.Target>
+              <UnstyledButton>
+                <Code fw="bold" className={classes.version}>
+                  <span>{packageJson.version}</span>
+                  <IconChevronDown className={classes.versionChevron} stroke={1.5} />
+                </Code>
+              </UnstyledButton>
+            </Menu.Target>
+
+            <Menu.Dropdown>{versionItems}</Menu.Dropdown>
+          </Menu>
         </div>
 
         <HeaderControls
