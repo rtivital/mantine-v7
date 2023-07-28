7b89e134c124dbf1dd22817ee36872aa4a7ce949
Vitaly Rtishchev <rtivital@gmail.com>
2023-07-28 13:42:12 +0400

    Add custom action spotlight demo

--- src/mantine-demos/src/demos/spotlight/index.ts
+++ src/mantine-demos/src/demos/spotlight/index.ts
@@ -2,3 +2,4 @@ export { usage } from './Spotlight.demo.usage';
 export { compound } from './Spotlight.demo.compound';
 export { scrollable } from './Spotlight.demo.scrollable';
 export { limit } from './Spotlight.demo.limit';
+export { customAction } from './Spotlight.demo.customAction';
