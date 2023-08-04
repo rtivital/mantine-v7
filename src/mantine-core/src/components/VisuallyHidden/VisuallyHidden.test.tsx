49a1707fc3b0af1aafc9d07e2cc8aa6ca688d9aa
Vitaly Rtishchev <rtivital@gmail.com>
2023-08-04 17:23:38 +0400

    Migrate VisuallyHidden to factory

--- src/mantine-core/src/components/VisuallyHidden/VisuallyHidden.test.tsx
+++ src/mantine-core/src/components/VisuallyHidden/VisuallyHidden.test.tsx
@@ -1,14 +1,20 @@
 import { tests } from '@mantine/tests';
-import { VisuallyHidden, VisuallyHiddenProps } from './VisuallyHidden';
+import { VisuallyHidden, VisuallyHiddenProps, VisuallyHiddenStylesNames } from './VisuallyHidden';
 
 const defaultProps: VisuallyHiddenProps = {};
 
 describe('@mantine/core/VisuallyHidden', () => {
-  tests.itSupportsClassName({ component: VisuallyHidden, props: defaultProps });
-  tests.itRendersChildren({ component: VisuallyHidden, props: defaultProps });
-  tests.itHasClasses({ component: VisuallyHidden });
-
-  it('has correct displayName', () => {
-    expect(VisuallyHidden.displayName).toEqual('@mantine/core/VisuallyHidden');
+  tests.itSupportsSystemProps<VisuallyHiddenProps, VisuallyHiddenStylesNames>({
+    component: VisuallyHidden,
+    props: defaultProps,
+    styleProps: true,
+    children: true,
+    extend: true,
+    variant: true,
+    size: true,
+    classes: true,
+    refType: HTMLSpanElement,
+    displayName: '@mantine/core/VisuallyHidden',
+    stylesApiSelectors: ['root'],
   });
 });
--- src/mantine-core/src/components/VisuallyHidden/VisuallyHidden.tsx
+++ src/mantine-core/src/components/VisuallyHidden/VisuallyHidden.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
-import cx from 'clsx';
+import {
+  Box,
+  BoxProps,
+  StylesApiProps,
+  factory,
+  ElementProps,
+  useProps,
+  useStyles,
+  Factory,
+} from '../../core';
 import classes from './VisuallyHidden.module.css';
 
-export interface VisuallyHiddenProps extends React.ComponentPropsWithoutRef<'span'> {}
+export type VisuallyHiddenStylesNames = 'root';
 
-export function VisuallyHidden({ className, ...others }: VisuallyHiddenProps) {
-  return <span className={cx(classes.root, className)} {...others} />;
-}
+export interface VisuallyHiddenProps
+  extends BoxProps,
+    StylesApiProps<VisuallyHiddenFactory>,
+    ElementProps<'div'> {}
+
+export type VisuallyHiddenFactory = Factory<{
+  props: VisuallyHiddenProps;
+  ref: HTMLDivElement;
+  stylesNames: VisuallyHiddenStylesNames;
+}>;
+
+const defaultProps: Partial<VisuallyHiddenProps> = {};
+
+export const VisuallyHidden = factory<VisuallyHiddenFactory>((_props, ref) => {
+  const props = useProps('VisuallyHidden', defaultProps, _props);
+  const { classNames, className, style, styles, unstyled, vars, ...others } = props;
+
+  const getStyles = useStyles<VisuallyHiddenFactory>({
+    name: 'VisuallyHidden',
+    classes,
+    props,
+    className,
+    style,
+    classNames,
+    styles,
+    unstyled,
+  });
+
+  return <Box component="span" ref={ref} {...getStyles('root')} {...others} />;
+});
 
 VisuallyHidden.classes = classes;
 VisuallyHidden.displayName = '@mantine/core/VisuallyHidden';
