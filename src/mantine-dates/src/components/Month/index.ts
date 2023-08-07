b6f04dbdc0f3a8ab0a67d73a955195cd4c128ea7
Vitaly Rtishchev <rtivital@gmail.com>
2023-08-07 12:41:54 +0400

    Add HiddenDatesInput component

---
+++ src/mantine-dates/src/components/Month/index.ts
@@ -0,0 +1,13 @@
+export { getEndOfWeek } from './get-end-of-week/get-end-of-week';
+export { getStartOfWeek } from './get-start-of-week/get-start-of-week';
+export { getMonthDays } from './get-month-days/get-month-days';
+export { isSameMonth } from './is-same-month/is-same-month';
+
+export { Month } from './Month';
+export type {
+  MonthProps,
+  MonthSettings,
+  MonthStylesNames,
+  MonthCssVariables,
+  MonthFactory,
+} from './Month';
