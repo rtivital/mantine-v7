18acbcc10f8b0c8ae9d0e6c4c936ed7dc9d23e85
Vitaly Rtishchev <rtivital@gmail.com>
2023-08-17 22:44:08 +0400

    Fix incorrect NumberInput demos

--- src/mantine-demos/src/demos/core/NumberInput/NumberInput.demo.minMax.tsx
+++ src/mantine-demos/src/demos/core/NumberInput/NumberInput.demo.minMax.tsx
@@ -8,11 +8,10 @@ import { NumberInput } from '@mantine/core';
 function Demo() {
   return (
     <NumberInput
-      label="Always show 2 digits after decimal point"
-      placeholder="Do not enter more that 2"
-      decimalScale={2}
-      fixedDecimalScale
-      defaultValue={2.2}
+      label="Enter value between 10 and 20"
+      placeholder="Don't enter more than 20 and less than 10"
+      min={10}
+      max={20}
     />
   );
 }
