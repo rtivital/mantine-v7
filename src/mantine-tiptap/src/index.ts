7e59d5f92adab64248c5a5dd5001321095539efa
Vitaly Rtishchev <rtivital@gmail.com>
2023-08-17 15:04:09 +0400

    Add RichTextEditor exports

--- src/mantine-tiptap/src/index.ts
+++ src/mantine-tiptap/src/index.ts
@@ -2,3 +2,8 @@ export * from './extensions';
 export { RichTextEditor } from './RichTextEditor';
 export { useRichTextEditorContext } from './RichTextEditor.context';
 export { DEFAULT_LABELS } from './labels';
+
+export * from './RichTextEditorControl';
+export { RichTextEditorControlsGroup } from './RichTextEditorControlsGroup/RichTextEditorControlsGroup';
+export { RichTextEditorControl } from './RichTextEditorControl/RichTextEditorControl';
+export { RichTextEditorContent } from './RichTextEditorContent/RichTextEditorContent';
