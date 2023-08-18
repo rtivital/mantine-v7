a3baf144f4781032cfec7188bdbe717bd8c09aed
Vitaly Rtishchev <rtivital@gmail.com>
2023-08-18 21:20:34 +0400

    Add Waves to the home page

--- docs/components/HomePage/HomePage.tsx
+++ docs/components/HomePage/HomePage.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import Head from 'next/head';
 import { Shell } from '../Shell';
 import { Footer } from '../Footer';
 import { Jumbotron } from './Jumbotron/Jumbotron';
+import { Waves } from './Waves/Waves';
 import classes from './HomePage.module.css';
 
 export function HomePage() {
   return (
-    <Shell withNavbar={false}>
-      <div className={classes.root}>
-        <Jumbotron />
-      </div>
-      <Footer />
-    </Shell>
+    <>
+      <Head>
+        <title>Mantine</title>
+      </Head>
+      <Shell withNavbar={false}>
+        <div className={classes.root}>
+          <Jumbotron />
+          <Waves height={40} width={150} />
+        </div>
+        <Footer />
+      </Shell>
+    </>
   );
 }
