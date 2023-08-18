9a1db6574cbb4432e0fb697cb7d4e2b38cd36210
Vitaly Rtishchev <rtivital@gmail.com>
2023-08-18 15:47:22 +0400

    Init home page

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import Link from 'next/link';
-import { Stack, Button } from '@mantine/core';
-import { MdxTitle } from '@/components/MdxProvider';
+import { HomePage } from '@/components/HomePage';
 
 export default function Home() {
-  return (
-    <>
-      <Stack align="center" mt={200}>
-        <MdxTitle order={1}>Imagine a home page here</MdxTitle>
-        <Button component={Link} href="/getting-started" size="xl" radius="md">
-          Get started with 7.x
-        </Button>
-      </Stack>
-    </>
-  );
+  return <HomePage />;
 }
