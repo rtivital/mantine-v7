f3a215d660e42911df4d78578b725274d4f5bbfc
Vitaly Rtishchev <rtivital@gmail.com>
2023-08-27 15:52:16 -0300

    Update ActionIcon demo to include icon size

--- src/mantine-demos/src/demos/core/ActionIcon/ActionIcon.demo.usage.tsx
+++ src/mantine-demos/src/demos/core/ActionIcon/ActionIcon.demo.usage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MantineDemo } from '@mantine/ds';
 import { IconAdjustments } from '@tabler/icons-react';
-import { ActionIcon, rem } from '@mantine/core';
+import { ActionIcon } from '@mantine/core';
 import { interactiveVariantsControl } from '../../../shared';
 
 const code = `
@@ -11,27 +11,16 @@ import { IconAdjustments } from '@tabler/icons-react';
 function Demo() {
   return (
     <ActionIcon{{props}} aria-label="Settings">
-      <IconAdjustments />
+      <IconAdjustments style={{ width: '70%', height: '70%' }} stroke={1.5} />
     </ActionIcon>
   );
 }
 `;
 
-const iconSizes: Record<string, string> = {
-  xs: rem(12),
-  sm: rem(14),
-  md: rem(18),
-  lg: rem(26),
-  xl: rem(34),
-};
-
 function Wrapper(props: any) {
   return (
     <ActionIcon {...props} aria-label="Settings">
-      <IconAdjustments
-        style={{ width: iconSizes[props.size], height: iconSizes[props.size] }}
-        stroke={1.5}
-      />
+      <IconAdjustments style={{ width: '70%', height: '70%' }} stroke={1.5} />
     </ActionIcon>
   );
 }
