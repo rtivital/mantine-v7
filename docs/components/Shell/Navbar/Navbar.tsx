47113b3fca4d1440a21d12ccf42e22bca20f1a47
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-12 15:34:24 +0400

    Fix navbar not closing when main link is clicked in docs navbar

--- docs/components/Shell/Navbar/Navbar.tsx
+++ docs/components/Shell/Navbar/Navbar.tsx
@@ -34,30 +34,35 @@ export function Navbar({ navbarOpened, onNavbarClose, mobileNavbarOnly }: Navbar
           <NavbarMainLink
             icon={<IconCompass style={{ width: rem(25), height: rem(25) }} stroke={1.5} />}
             href="/getting-started"
+            onNavbarClose={onNavbarClose}
           >
             Getting started
           </NavbarMainLink>
           <NavbarMainLink
             icon={<IconBrandMantine style={{ width: rem(25), height: rem(25) }} stroke={1.5} />}
             href="/about"
+            onNavbarClose={onNavbarClose}
           >
             About Mantine
           </NavbarMainLink>
           <NavbarMainLink
             icon={<IconApi style={{ width: rem(25), height: rem(25) }} stroke={1.5} />}
             href="/overview"
+            onNavbarClose={onNavbarClose}
           >
             API Overview
           </NavbarMainLink>
           <NavbarMainLink
             icon={<IconHeartHandshake style={{ width: rem(25), height: rem(25) }} stroke={1.5} />}
             href="/contribute"
+            onNavbarClose={onNavbarClose}
           >
             Contribute
           </NavbarMainLink>
           <NavbarMainLink
             icon={<IconPalette style={{ width: rem(22), height: rem(22) }} stroke={1.5} />}
             href="/colors-generator"
+            onNavbarClose={onNavbarClose}
           >
             Colors generator
           </NavbarMainLink>
@@ -65,6 +70,7 @@ export function Navbar({ navbarOpened, onNavbarClose, mobileNavbarOnly }: Navbar
             external
             icon={<IconBrandMantine style={{ width: rem(22), height: rem(22) }} stroke={1.5} />}
             href="https://ui.mantine.dev"
+            onNavbarClose={onNavbarClose}
           >
             Mantine UI
           </NavbarMainLink>
--- docs/components/Shell/Navbar/NavbarMainLink/NavbarMainLink.tsx
+++ docs/components/Shell/Navbar/NavbarMainLink/NavbarMainLink.tsx
@@ -8,9 +8,16 @@ interface NavbarMainLinkProps extends React.ComponentPropsWithoutRef<'a'> {
   external?: boolean;
   children: React.ReactNode;
   icon: React.ReactNode;
+  onNavbarClose(): void;
 }
 
-export function NavbarMainLink({ href, external, children, icon }: NavbarMainLinkProps) {
+export function NavbarMainLink({
+  href,
+  external,
+  children,
+  icon,
+  onNavbarClose,
+}: NavbarMainLinkProps) {
   const router = useRouter();
   return (
     <UnstyledButton<any>
@@ -19,6 +26,7 @@ export function NavbarMainLink({ href, external, children, icon }: NavbarMainLin
       target={external ? '_blank' : undefined}
       className={classes.link}
       mod={{ active: router.pathname === href }}
+      onClick={onNavbarClose}
     >
       <span className={classes.icon}>{icon}</span>
       <span className={classes.label}>{children}</span>
